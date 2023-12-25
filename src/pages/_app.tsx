33a85944780bc7be1907bdec3d0334e89e0a83f7
jianrong7 <ipwnjianrong@gmail.com>
2023-12-25 17:15:15 +0800

    remove clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,17 @@ import { Toaster } from "@/components/ui/toaster";
 import useAuthStore from "@/stores/useAuthStore";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ClerkProvider } from "@clerk/nextjs";
+// import { ClerkProvider } from "@clerk/nextjs";
 
 export default function App({ Component, pageProps }: AppProps) {
   const { isLoggedIn } = useAuthStore();
   return (
-    <ClerkProvider {...pageProps}>
+    // <ClerkProvider {...pageProps}>
+    <>
       {isLoggedIn && <InactivityDialog />}
       <Component {...pageProps} />
       <Toaster />
-    </ClerkProvider>
+    </>
+    // </ClerkProvider>
   );
 }
