0713d415421bb8983cddaffa85bc1ae55055fb04
James <jamessinmaojun@gmail.com>
2023-12-27 08:15:16 +0800

    Fixed email editor logic.

---
+++ src/lib/useRouteHandler.ts
@@ -0,0 +1,49 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+type routeHandlerProps = {
+  isEditing: boolean;
+  setIsEditing: Function;
+  didSaveContent: boolean;
+  setDidSaveContent: Function;
+}
+
+const useRouteHandler = ({ isEditing, setIsEditing, didSaveContent, setDidSaveContent }: routeHandlerProps) => {
+  const router = useRouter()
+
+  const [routeAwayConfirmationOpen, setRouteAwayConfirmationOpen] = useState(false)
+  const [routeAwayUrl, setRouteAwayUrl] = useState("")
+
+  const navigate = (path: string) => {
+    return router.push(path)
+  }
+
+  useEffect(() => {
+    setDidSaveContent(false)
+    const handleRouteChange = (url: string) => {
+      // if re-routing back to current page or user is not editing.
+      if (url == '/email-editor' || !isEditing) {
+        return
+      }
+      if (didSaveContent) {
+        // Continue with the navigation
+        setIsEditing(false)
+      } else {
+        // Cancel the navigation
+        setRouteAwayConfirmationOpen(true)
+        setRouteAwayUrl(url)
+        throw('cancelled route change')
+      }
+    }
+
+    router.events.on('routeChangeStart', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange)
+    }
+  }, [router.events, isEditing])
+
+  return { navigate, routeAwayUrl, routeAwayConfirmationOpen, setRouteAwayConfirmationOpen }
+}
+
+export default useRouteHandler
