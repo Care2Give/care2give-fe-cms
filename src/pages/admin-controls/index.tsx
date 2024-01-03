25488e790c15ab5ad53356ff5f5c80d12b6a058e
jianrong7 <ipwnjianrong@gmail.com>
2024-01-03 11:06:07 +0800

    fix typo

--- src/pages/admin-controls/index.tsx
+++ src/pages/admin-controls/index.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/admin-controls/Header";
 import Table from "@/components/admin-controls/table";
 import Layout from "@/components/layout";
 import useIsLoggedIn from "@/lib/useIsLoggedIn";
-import useIsSuperuser from "@/lib/useIsSuperUser";
+import useIsSuperuser from "@/lib/useIsSuperuser";
 
 export default function AdminControls() {
   useIsLoggedIn();
