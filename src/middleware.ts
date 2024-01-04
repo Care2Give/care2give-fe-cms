791bcc654a96dcff5f0bb5a4c0596fe26aa9ff1a
jianrong7 <ipwnjianrong@gmail.com>
2024-01-04 17:44:29 +0800

    secure routes using middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,32 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+
+const SUPERUSER_PATHS = ["/admin-controls", "/api/users"];
+const DONATION_MANAGER_PATHS = ["/email-editor", "/donations"];
+
 export default authMiddleware({
+  afterAuth(auth, req, evt) {
+    const role = (auth?.sessionClaims?.public_metadata as { role: string })
+      ?.role;
+    if (
+      role !== "superuser" &&
+      SUPERUSER_PATHS.includes(req.nextUrl.pathname)
+    ) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+
+    if (
+      role !== "donation-manager" &&
+      role !== "superuser" &&
+      DONATION_MANAGER_PATHS.includes(req.nextUrl.pathname)
+    ) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  },
   publicRoutes: ["/login"],
   // debug: true,
 });
--- src/pages/admin-controls/index.tsx
+++ src/pages/admin-controls/index.tsx
@@ -1,12 +1,8 @@
 import Header from "@/components/admin-controls/Header";
 import Table from "@/components/admin-controls/table";
 import Layout from "@/components/layout";
-import useClerkSWR from "@/lib/useClerkSWR";
-import useIsSuperuser from "@/lib/useIsSuperuser";
 
 export default function AdminControls() {
-  // useIsSuperuser();
-
   return (
     <Layout>
       <Header />
