e53040dc6019d06816547fc9a985d487c5f07e90
yucheng2207 <e0560244@u.nus.edu>
2024-01-07 21:45:29 +0800

    update campaign to use editStage

--- src/components/campaigns/edit/SubHeader.tsx
+++ src/components/campaigns/edit/SubHeader.tsx
@@ -1,6 +1,7 @@
-import { arabotoBold } from "@/lib/font";
-import { Montserrat } from "next/font/google";
-import BreadCrumbs from "@/components/ui/breadcrumbs";
+import {arabotoBold} from "@/lib/font";
+import {Montserrat} from "next/font/google";
+import BreadCrumbs, {Crumb} from "@/components/ui/breadcrumbs";
+import { EditStage, editStageOrdering, getDisplay } from "@/pages/campaigns/edit/edit-stage"
 
 const montserrat = Montserrat({
     subsets: ["latin"],
@@ -8,19 +9,24 @@ const montserrat = Montserrat({
     weight: ["700"],
 });
 
-export default function SubHeader() {
-    const crumbs = [
-        {display: "Edit", link: "edit"},
-        {display: "Preview", link: "preview"},
-        {display: "Publish", link: "publish"}
-    ];
+export default function SubHeader({editStage, setEditStage}: {
+    editStage: EditStage,
+    setEditStage: (EditStage) => void,
+}) {
+    const crumbs = editStageOrdering.map((curEditStage: EditStage) => {
+        return ({
+            display: getDisplay(curEditStage),
+            onClick: () => setEditStage(curEditStage)
+        });
+    })
+
     return (
         <div className="flex justify-between items-center px-2">
             <div>
                 <p className={`${arabotoBold.className} text-2xl`}>New Campaign</p>
             </div>
             <div>
-                <BreadCrumbs allCrumbs={crumbs} currentCrumb={{display: "Edit", link: "edit"}} separator={">"}/>
+                <BreadCrumbs allCrumbs={crumbs} currentCrumbIndex={editStage} separator={">"}/>
             </div>
         </div>
     );
--- src/pages/campaigns/edit/index.tsx
+++ src/pages/campaigns/edit/index.tsx
@@ -1,12 +1,19 @@
 import Layout from "@/components/layout";
 import Header from "@/components/campaigns/Header";
 import SubHeader from "@/components/campaigns/edit/SubHeader"
+import Footer from "@/components/campaigns/edit/Footer";
+import {useState} from "react";
+import { EditStage } from "./edit-stage";
+
 export default function Index() {
+    const [editStage, setEditStage] = useState(EditStage.Edit);
+
     return (
         <Layout>
             <Header />
-            <SubHeader />
+            <SubHeader editStage={editStage} setEditStage={setEditStage}/>
             <div>This is the page to edit campaigns</div>
+            <Footer editStage={editStage} setEditStage={setEditStage}/>
         </Layout>
     );
 }
