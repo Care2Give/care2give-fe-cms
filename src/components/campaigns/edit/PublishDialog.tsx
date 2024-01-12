c2444c6a72962595833973e28ba54268e234a6c6
yucheng2207 <e0560244@u.nus.edu>
2024-01-12 16:22:21 +0800

    add functionality to publish campaign to BE

--- src/components/campaigns/edit/PublishDialog.tsx
+++ src/components/campaigns/edit/PublishDialog.tsx
@@ -7,11 +7,50 @@ import {
 } from "@/components/ui/dialog"
 import {useRouter} from "next/router";
 import { EditStage } from "@/pages/campaigns/edit/edit-stage";
+import useCampaignEditorStore from "@/stores/useCampaignEditorStore";
+import {useAuth} from "@clerk/nextjs";
 
 export function PublishDialog({setEditStage}) {
     const router = useRouter();
+    const campaignEditState = useCampaignEditorStore.getState();
+    const { getToken, userId } = useAuth();
 
-    // TODO add publish campaign function which pings BE
+    const publishCampaign = () => {
+        const campaignId = campaignEditState.id;
+        const isEdit = campaignId !== "";
+        const bodyObj = {
+            status: campaignEditState.status,
+            startDate: campaignEditState.startDate.toISOString(),
+            endDate: campaignEditState.endDate.toISOString(),
+            title: campaignEditState.title,
+            description: campaignEditState.description,
+            currency: "SGD",
+            dollars: Math.floor(campaignEditState.targetAmount),
+            cents: campaignEditState.targetAmount - Math.floor(campaignEditState.targetAmount),
+            editedBy: userId,
+            // TODO change to not convert CampaignImage to just its URL when database is updated
+            imageUrl: campaignEditState.images.map((campaignImage) => campaignImage.url),
+        }
+
+        if (!isEdit) {
+            bodyObj.createdBy = userId;
+        }
+
+        getToken().then((token) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_URL}/v1/campaign/${isEdit ? campaignId : "create"}`, {
+                method: isEdit ? "PATCH" : "POST",
+                headers: { Authorization: `Bearer ${token}`, "Content-Type": "application/json",},
+                body: JSON.stringify(bodyObj),
+            }).then((res) => res.json()).then(response => {
+                router.push("/campaigns");
+            }).catch(error => {
+
+            })
+        })
+
+        // TODO add functionality to create the donation amount - current missing in BE
+    }
+
     return (
         <Dialog open={true}>
             <DialogContent className="sm:max-w-[430px]">
@@ -20,7 +59,7 @@ export function PublishDialog({setEditStage}) {
                 </DialogHeader>
                 <div className="flex justify-around">
                     <Button onClick={() => setEditStage(EditStage.Edit)} variant="ghost" className="border">Cancel</Button>
-                    <Button onClick={() => router.push("/campaigns")}>Confirm</Button>
+                    <Button onClick={publishCampaign}>Confirm</Button>
                 </div>
             </DialogContent>
         </Dialog>
