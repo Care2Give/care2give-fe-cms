e7edbc9a3592dff6e311d4bb51aceca81c0b8984
jianrong7 <ipwnjianrong@gmail.com>
2024-01-12 22:38:07 +0800

    fix footer css

--- src/components/campaigns/edit/Footer.tsx
+++ src/components/campaigns/edit/Footer.tsx
@@ -10,13 +10,13 @@ import { useRouter } from "next/router";
 export default function Footer({ editStage }: { editStage: EditStage }) {
   const router = useRouter();
   // TODO change this to get actual time
-  const lastSaved: string = "1 min";
+  // const lastSaved: string = "1 min";
 
   return (
-    <footer className="fixed bottom-0 z-20 w-5/6 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+    <footer className="py-6 px-24 fixed bottom-0 left-48 w-11/12 z-20 bg-white border-t border-gray-200 shadow flex items-center justify-between">
       <span className="flex flex-wrap items-center text-sm text-gray-500 sm:text-center dark:text-gray-400">
         <CheckCircle2 />
-        <span className="px-2">Last saved {lastSaved} ago.</span>
+        {/* <span className="px-2">Last saved {lastSaved} ago.</span> */}
       </span>
       <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li className="px-2">
@@ -24,7 +24,7 @@ export default function Footer({ editStage }: { editStage: EditStage }) {
             Back
           </Button>
         </li>
-        <li className="px-2">
+        <li>
           <Button
             className="bg-[#5185FF] hover:bg-[#3872FC]"
             type="submit"
