7baf708ba77b0ee617990eef25d0454dd6ac53e1
jianrong7 <ipwnjianrong@gmail.com>
2024-01-12 22:32:35 +0800

    fix calendar component

--- src/components/campaigns/edit/FormFieldDatePicker.tsx
+++ src/components/campaigns/edit/FormFieldDatePicker.tsx
@@ -1,86 +1,84 @@
-import React, { useEffect } from "react";
-import { get, UseFormReturn } from "react-hook-form";
+import { CalendarIcon } from "@radix-ui/react-icons";
+import { addDays, format } from "date-fns";
+
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { FormControl, FormLabel } from "@/components/ui/form";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { addDays, format } from "date-fns";
-import { cn } from "@/lib/utils";
-import { Calendar } from "@/components/ui/calendar";
-import { CalendarIcon } from "lucide-react";
-import useCampaignEditorStore from "@/stores/useCampaignEditorStore";
 import { DateRange } from "react-day-picker";
+import { useState } from "react";
+import useCampaignEditorStore from "@/stores/useCampaignEditorStore";
 
-export default function FormFieldDatePicker({ form }: { form: UseFormReturn }) {
-  const [date, setDate] = React.useState<DateRange>({
-    from: new Date(),
-    to: addDays(new Date(), 20),
+export default function DatePickerForm() {
+  const [date, setDate] = useState<DateRange | undefined>({
+    from: new Date(2024, 0, 20),
+    to: addDays(new Date(2024, 0, 20), 20),
   });
 
-  const setEndDate = useCampaignEditorStore((state) => state.setEndDate);
-  const setStartDate = useCampaignEditorStore((state) => state.setStartDate);
-
-  useEffect(() => {
-    form.setValue("startDate", date?.from);
-    form.setValue("endDate", date?.to);
-    setEndDate(date.to as Date);
-    setStartDate(date.from as Date);
-  }, [JSON.stringify(date)]);
+  const { setEndDate, setStartDate } = useCampaignEditorStore();
 
   return (
-    <div className="my-2 flex flex-row items-center justify-between">
-      <FormLabel className="text-base ps-10">Duration</FormLabel>
-      <Popover>
-        <span className="w-1/2">
-          <PopoverTrigger asChild>
-            <FormControl>
-              <Button
-                id="date"
-                variant={"outline"}
-                className={cn(
-                  "w-full justify-start text-left font-normal",
-                  !date && "text-muted-foreground"
-                )}
-              >
-                <CalendarIcon className="mr-2 h-4 w-4" />
-                {date?.from ? (
-                  date.to ? (
-                    <>
-                      {format(date.from, "LLL dd, y")} -{" "}
-                      {format(date.to, "LLL dd, y")}
-                    </>
-                  ) : (
-                    format(date.from, "LLL dd, y")
-                  )
-                ) : (
-                  <span>Pick a date</span>
-                )}
-              </Button>
-            </FormControl>
-          </PopoverTrigger>
-          {form.formState.errors && get(form.formState.errors, "startDate") && (
-            <div>
-              From date: {get(form.formState.errors, "startDate").message}
-            </div>
-          )}
-          {form.formState.errors && get(form.formState.errors, "endDate") && (
-            <div>To date: {get(form.formState.errors, "endDate").message}</div>
-          )}
-        </span>
-        <PopoverContent className="w-auto p-0" align="start">
-          <Calendar
-            initialFocus
-            mode="range"
-            defaultMonth={date?.from}
-            selected={date}
-            onSelect={setDate}
-            numberOfMonths={2}
-          />
-        </PopoverContent>
-      </Popover>
+    <div className="my-2">
+      <FormField
+        name="duration"
+        render={({ field }) => (
+          <FormItem className="flex flex-row items-center justify-between">
+            <FormLabel className="text-base ps-10">Duration</FormLabel>
+            <Popover>
+              <PopoverTrigger asChild>
+                <FormControl>
+                  <Button
+                    variant={"outline"}
+                    className={cn(
+                      "w-[240px] pl-3 text-left font-normal",
+                      !field.value && "text-muted-foreground"
+                    )}
+                  >
+                    <CalendarIcon className="mr-auto h-4 w-4 opacity-50" />
+                    {date?.from ? (
+                      date.to ? (
+                        <>
+                          {format(date.from, "LLL dd, y")} -{" "}
+                          {format(date.to, "LLL dd, y")}
+                        </>
+                      ) : (
+                        format(date.from, "LLL dd, y")
+                      )
+                    ) : (
+                      <span>Pick a date</span>
+                    )}
+                  </Button>
+                </FormControl>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0" align="start">
+                <Calendar
+                  mode="range"
+                  defaultMonth={date?.from}
+                  selected={date}
+                  onSelect={(e) => {
+                    setDate(e);
+                    setStartDate(e?.from as Date);
+                    setEndDate(e?.to as Date);
+                  }}
+                  numberOfMonths={2}
+                  disabled={(date) => date < addDays(new Date(), -365)}
+                  initialFocus
+                />
+              </PopoverContent>
+            </Popover>
+          </FormItem>
+        )}
+      />
     </div>
   );
 }
--- src/stores/useCampaignEditorStore.ts
+++ src/stores/useCampaignEditorStore.ts
@@ -1,61 +1,62 @@
-import {create} from "zustand";
-import {CampaignDonationAmount, CampaignStatus} from "@/types/prismaSchema";
+import { create } from "zustand";
+import { CampaignDonationAmount, CampaignStatus } from "@/types/prismaSchema";
 
 export type CampaignImage = {
-    url: string;
-    name: string;
-}
+  url: string;
+  name: string;
+};
 
 interface CampaignEditorState {
-    id: string,
-    setId: (id: string) => void;
-    status: CampaignStatus;
-    setStatus: (status: CampaignStatus) => void;
-    title: string;
-    setTitle: (title: string) => void;
-    description: string;
-    setDescription: (description: string) => void;
-    targetAmount: number;
-    setTargetAmount: (targetAmount: number) => void;
-    startDate: Date;
-    setStartDate: (startDate: Date) => void;
-    endDate: Date;
-    setEndDate: (endDate: Date) => void;
-    donationOptions: CampaignDonationAmount[];
-    setDonationOptions: (donationOptions: CampaignDonationAmount[]) => void;
-    images: CampaignImage[];
-    setImages: (images: CampaignImage[]) => void;
-    reset: () => void;
+  id: string;
+  setId: (id: string) => void;
+  status: CampaignStatus;
+  setStatus: (status: CampaignStatus) => void;
+  title: string;
+  setTitle: (title: string) => void;
+  description: string;
+  setDescription: (description: string) => void;
+  targetAmount: number;
+  setTargetAmount: (targetAmount: number) => void;
+  startDate: Date | null;
+  setStartDate: (startDate: Date) => void;
+  endDate: Date | null;
+  setEndDate: (endDate: Date) => void;
+  donationOptions: CampaignDonationAmount[];
+  setDonationOptions: (donationOptions: CampaignDonationAmount[]) => void;
+  images: CampaignImage[];
+  setImages: (images: CampaignImage[]) => void;
+  reset: () => void;
 }
 
 const useCampaignEditorStore = create<CampaignEditorState>()((set) => ({
-    id: "",
-    setId: (id) => set({id}),
-    status: CampaignStatus.ACTIVE,
-    setStatus: (status) => set({ status }),
-    title: "",
-    setTitle: (title) => set({title}),
-    description: "",
-    setDescription: (description) => set({description}),
-    targetAmount: 0,
-    setTargetAmount: (targetAmount) => set({targetAmount}),
-    startDate: new Date(),
-    setStartDate: (startDate) => set({startDate}),
-    endDate: new Date(),
-    setEndDate: (endDate) => set({endDate}),
-    donationOptions: [],
-    setDonationOptions: (donationOptions) => set({donationOptions}),
-    images: [],
-    setImages: (images: CampaignImage[]) => set({images}),
-    reset: () => set({
-        status: CampaignStatus.ACTIVE,
-        title: "",
-        description: "",
-        targetAmount: 0,
-        startDate: new Date(),
-        endDate: new Date(),
-        donationOptions: [],
-        images: [],
+  id: "",
+  setId: (id) => set({ id }),
+  status: CampaignStatus.ACTIVE,
+  setStatus: (status) => set({ status }),
+  title: "",
+  setTitle: (title) => set({ title }),
+  description: "",
+  setDescription: (description) => set({ description }),
+  targetAmount: 0,
+  setTargetAmount: (targetAmount) => set({ targetAmount }),
+  startDate: null,
+  setStartDate: (startDate) => set({ startDate }),
+  endDate: null,
+  setEndDate: (endDate) => set({ endDate }),
+  donationOptions: [],
+  setDonationOptions: (donationOptions) => set({ donationOptions }),
+  images: [],
+  setImages: (images: CampaignImage[]) => set({ images }),
+  reset: () =>
+    set({
+      status: CampaignStatus.ACTIVE,
+      title: "",
+      description: "",
+      targetAmount: 0,
+      startDate: new Date(),
+      endDate: new Date(),
+      donationOptions: [],
+      images: [],
     }),
 }));
 
