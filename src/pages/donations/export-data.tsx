93165364c37c207a7cdabbfee8bb9159a1fb24b7
jianrong7 <ipwnjianrong@gmail.com>
2024-01-15 22:20:25 +0800

    integrate export excel endpoint with fe

--- src/pages/donations/export-data.tsx
+++ src/pages/donations/export-data.tsx
@@ -5,8 +5,10 @@ import useDonationStore from "@/stores/useDonationStore";
 import { Button } from "@/components/ui/button";
 import Header from "@/components/donations/Header";
 import useClerkSWR from "@/lib/useClerkSWR";
-import { convertTitleToValueAndLabel } from "@/lib/utils";
+import { convertTitleToValueAndLabel, dateOptions } from "@/lib/utils";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useAuth } from "@clerk/nextjs";
+import { saveAs } from "file-saver";
 
 export type CampaignInput = {
   value: string;
@@ -26,22 +28,35 @@ export default function ExportData() {
     `${process.env.NEXT_PUBLIC_API_URL}/v1/cms/donations/campaigns`
   );
 
+  const { getToken } = useAuth();
+
   const handleExportData = async () => {
-    // TODO: Integrate BE endpoint to export data
-    // const res = await fetch(
-    //   `${process.env.NEXT_PUBLIC_API_URL}/v1/cms/donations/export`,
-    //   {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify({
-    //       startDate,
-    //       endDate,
-    //       campaigns: campaignsToExport,
-    //     }),
-    //   }
-    // );
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/v1/cms/donations/export`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${await getToken()}`,
+        },
+        body: JSON.stringify({
+          startDate,
+          endDate,
+          campaignIds: campaignsToExport.map((c) => c.value),
+        }),
+      }
+    );
+    const data = await res.arrayBuffer();
+
+    saveAs(
+      new Blob([data], {
+        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      }),
+      `${new Date(startDate).toLocaleDateString(
+        "en-SG",
+        dateOptions
+      )}-${new Date(endDate).toLocaleDateString("en-SG", dateOptions)}.xlsx`
+    );
     return;
   };
 
