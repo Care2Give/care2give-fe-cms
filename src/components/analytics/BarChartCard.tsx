e69642d1965387fde1ba6ec9ac7f7d7023f5977f
jianrong7 <ipwnjianrong@gmail.com>
2024-01-23 13:16:57 +0800

    fix barchartcard types

--- src/components/analytics/BarChartCard.tsx
+++ src/components/analytics/BarChartCard.tsx
@@ -34,7 +34,7 @@ type BarChartCardProps = {
   tableDetails: CampaignProps[];
   Table: any;
   isLoading: boolean;
-  setFrequency: (string) => void
+  setFrequency: (arg0: string) => void;
 };
 
 export default function BarChartCard({
@@ -43,33 +43,37 @@ export default function BarChartCard({
   tableDetails,
   Table,
   isLoading,
-  setFrequency
+  setFrequency,
 }: BarChartCardProps) {
   return (
     <div className="bg-white shadow rounded flex flex-col p-8 m-2">
       <div className="flex justify-between">
         <div className={`${montserrat.className} text-1xl`}>{title}</div>
-        <CardFrequencySelector onChange={setFrequency}/>
+        <CardFrequencySelector onChange={setFrequency} />
       </div>
       <div className="flex mt-10">
-        {
-          isLoading
-            ? <Spinner />
-            : <>
-                <div className="h-full flex-initial mr-1">
-                  <BarChart {...barChartDetails} />
-                </div>
-                <div className="flex flex-1 ml-1 overflow-hidden">
-                  <Table {...tableDetails} />
-                </div>
-              </>
-        }
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <>
+            <div className="h-full flex-initial mr-1">
+              <BarChart {...barChartDetails} />
+            </div>
+            <div className="flex flex-1 ml-1 overflow-hidden">
+              <Table {...tableDetails} />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
 }
 
-function CardFrequencySelector({onChange}) {
+function CardFrequencySelector({
+  onChange,
+}: {
+  onChange: (arg0: string) => void;
+}) {
   return (
     <Select onValueChange={onChange}>
       <SelectTrigger className="w-[97px] h-[27px]">
